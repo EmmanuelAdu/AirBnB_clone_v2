e4150f770e21ad30329eb6a48d999bd28d1710d8
EmmanuelAdu <christorenzo20@gmail.com>
2023-12-04 10:05:24 +0000

    Allowing object creation with given parameters using `create` command on the console

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,51 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        attrs = ('id', 'created_at', 'updated_at', '__class__')
+        class_name = ''
+        o_kwargs = {}
+
+        # split the argument into className and param
+        arg_split = args.split(maxsplit=1)
+        class_name = arg_split[0]
+        param_str = arg_split[1] if len(arg_split) > 1 else ''
+
+        # split the parameters into key-value pairs
+        split_param = param_str.split()
+        for split_param in param_str.split():
+            key, val = split_param.split('=')
+            if val[0] == '"' and val[-1] == '"':
+                Value = val[1:-1].replace('\\"', '"').replace('_', ' ')
+            elif '.' in val:
+                # float value
+                try:
+                    Value = float(val)
+                except ValueError:
+                    continue
+            else:
+                # integer value
+                try:
+                    Value = int(val)
+                except ValueError:
+                    continue
+
+            # take key, value to dict
+            o_kwargs[key] = Value
+
+        if not class_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        new_instance = HBNBCommand.classes[class_name]()
+        for key, Value in o_kwargs.items():
+            if key not in attrs:
+                setattr(new_instance, key, Value)
+
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +307,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +315,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +354,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
