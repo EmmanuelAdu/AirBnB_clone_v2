94156f9f2771f3f1955148eab8345b6a83e2ea2e
EmmanuelAdu <christorenzo20@gmail.com>
2023-12-17 18:57:40 +0000

    Compressing All files to `versions` directory

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module contains function that compresses web_static folder
+"""
+
+from datetime import date
+from time import strftime
+from fabric.api import local
+import os
+
+def do_pack():
+    """Function to compress web_static folder"""
+    file_time = strftime("%Y%m%d%H%M%S")
+    
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        command = "tar -cvzf"
+        compressed = "versions/web_static_{}.tgz".format(file_time)
+        target = "web_static/"
+        local('{} {} {}'.format(command, compressed, target))
+        
+        return compressed
+    except Exception as e:
+        return None
