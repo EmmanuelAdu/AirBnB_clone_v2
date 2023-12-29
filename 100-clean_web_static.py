bec4bb62bc0d3ced9ad3d6eb06f04574dfa66618
EmmanuelAdu <christorenzo20@gmail.com>
2023-12-29 15:48:00 +0000

    Deleting Unnecessary archive files from versions

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
     -i ssh-key -u ubuntu > /dev/null 2>&1
 """
 import os
-from frabric import *
+from fabric.api import *
 
 
 env.hosts = ['35.153.18.211', '34.207.222.234']
@@ -15,12 +15,12 @@ def do_clean(number=0):
     """Function to delete archive files
     Arg:
         number (int) - number of archives to delete
-            > When 0 or 1 keep most recent version of archive
-            > When 2 keep most recent and 2-most recent
+    When 0 or 1 keep most recent version of archive
+    When 2 keep most recent and 2-most recent
     """
     number = 1 if int(number) == 0 else int(number)
-    archives = sorted(listdir("versions"))
-    [archives.pop() for i range(number)]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(files)) for files in archives]
 
