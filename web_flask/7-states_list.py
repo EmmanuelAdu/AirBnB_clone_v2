4c78764e41bb23e91346a93b76378d4abbe32e0c
EmmanuelAdu <christorenzo20@gmail.com>
2024-01-13 14:27:59 +0000

    Solution to task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Starts a web application
+starts a Flask web application
 """
-
 
 from flask import Flask, render_template
 from models import *
@@ -12,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display an HTML PAGE with list of states in alphabetical order"""
+    """display a HTML page with the states listed in alphabetical order"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
@@ -23,5 +22,5 @@ def teardown_db(exception):
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
